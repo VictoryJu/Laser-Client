e138d1fe932f1a8f43fb0bc5a6a0f8642c36101e
이지원 <gbwlxhd97@naver.com>
2022-04-25 17:41:33 +0900

    laser 관리자 모드 메인화면 퍼블리싱 완료

--- src/app/module/common/admin/component/admin-select-lane/admin-select-lane.component.ts
+++ src/app/module/common/admin/component/admin-select-lane/admin-select-lane.component.ts
@@ -1,3 +1,4 @@
+import { MatDialogRef } from '@angular/material/dialog';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminSelectLaneComponent implements OnInit {
 
-  constructor() { }
+  constructor(public _mat:MatDialogRef<AdminSelectLaneComponent>) { }
 
   ngOnInit(): void {
   }
 
+  closeLane() {
+    this._mat.close();
+  }
 }
