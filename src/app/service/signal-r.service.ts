e7f8bdf3ada96bf1cd72c64c32fec1f5582d77a2
win24-ju <85215390+win24-ju@users.noreply.github.com>
2022-04-27 16:56:57 +0900

    Laser signalR 설정중"
    "

---
+++ src/app/service/signal-r.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import * as signalR from "@microsoft/signalr";  // or from "@microsoft/signalr" if you are using a new library
+import { ChartModel } from '../interface/chartmodel';
+@Injectable({
+  providedIn: 'root'
+})
+export class SignalRService {
+  public data: ChartModel[];
+private hubConnection: signalR.HubConnection
+  public startConnection = () => {
+    this.hubConnection = new signalR.HubConnectionBuilder()
+                            .withUrl('https://localhost:22092/')
+                            .build();
+    this.hubConnection
+      .start()
+      .then(() => console.log('Connection started'))
+      .catch(err => console.log('Error while starting connection: ' + err))
+  }
+  public addTransferChartDataListener = () => {
+    this.hubConnection.on('transferchartdata', (data) => {
+      this.data = data;
+      console.log(data);
+    });
+  }
+}
