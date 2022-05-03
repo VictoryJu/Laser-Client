7358af373476928ac733e00ed8e13b976642ee0f
이지원 <gbwlxhd97@naver.com>
2022-05-03 16:39:29 +0900

    회원정보 수정 기능 추가

--- src/app/module/common/admin/component/admin-member-update/admin-member-update.component.ts
+++ src/app/module/common/admin/component/admin-member-update/admin-member-update.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-admin-member-update',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminMemberUpdateComponent implements OnInit {
 
-  constructor() { }
+  constructor(public _matRef:MatDialogRef<AdminMemberUpdateComponent>) { }
 
   ngOnInit(): void {
   }
 
+  close() {
+    this._matRef.close();
+  }
 }
